a62ec5a3c31a0d0f578109abed69bf556b16a63b
Heather Miller <heather.miller@epfl.ch>
2011-11-22 13:37:02 +0100

    Adding blocking (resizable) execution context.

--- src/library/scala/concurrent/ExecutionContext.scala
+++ src/library/scala/concurrent/ExecutionContext.scala
@@ -46,6 +46,24 @@ private[concurrent] class ForkJoinExecutionContext extends ExecutionContext {
   }
 }
 
+/**
+ * Implements a blocking execution context
+ */
+private[concurrent] class BlockingExecutionContext extends ExecutionContext {
+  val pool = makeCachedThreadPool // TODO FIXME: need to merge thread pool factory methods from Heather's parcolls repo
+
+  def execute(task: Runnable) {
+    val p = newPromise(task.run())
+    p.start()
+    pool execute p
+  }
+
+  // TODO FIXME: implement
+  def newPromise[T](body: => T): Promise[T] = {
+    throw new Exception("not yet implemented")
+  }
+}
+
 object ExecutionContext {
 
   private var _global: Option[ExecutionContext] = None
