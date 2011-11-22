8114028d65c3f35c819077128566cc19736d7d3f
Philipp Haller <hallerp@gmail.com>
2011-11-22 12:36:47 +0100

    Added Akka's futures. Started integrating execution contexts and promises.

--- src/library/scala/concurrent/ExecutionContext.scala
+++ src/library/scala/concurrent/ExecutionContext.scala
@@ -1,14 +1,64 @@
 package scala.concurrent
 
 import java.util.concurrent.{Executors, Future => JFuture}
+import scala.concurrent.forkjoin._
 
 trait ExecutionContext {
 
   def execute(task: Runnable): Unit
+  def newPromise[T](body: => T): Promise[T]
 
 }
 
+private[concurrent] class ForkJoinPromise extends DefaultPromise {
+
+  def start(): Unit = {
+    
+  }
+
+  // TODO FIXME: properly handle timeouts
+  override def await(atMost: Duration): this.type =
+    await
+
+  // TODO FIXME: implement
+  override def await: this.type = {
+    this
+  }
+
+  // TODO FIXME: add def tryCancel
+}
+
+private[concurrent] class ForkJoinExecutionContext extends ExecutionContext {
+  val pool = new ForkJoinPool
+
+  def execute(task: Runnable) {
+    val p = newPromise(task.run())
+
+    if (Thread.currentThread.isInstanceOf[ForkJoinWorkerThread])
+      p.start()
+    else
+      pool execute p
+  }
+
+  // TODO FIXME: implement
+  def newPromise[T](body: => T): Promise[T] = {
+    throw new Exception("not yet implemented")
+  }
+}
+
 object ExecutionContext {
+
+  private var _global: Option[ExecutionContext] = None
+
+  def global: ExecutionContext = {
+    if (_global.isEmpty) {
+      val context = new ExecutionContext {
+        def execute(task: Runnable) {
+          
+        }
+      }
+    }
+  }
 
   /* a Java ExecutorService which collects executed tasks in a HashMap, so that
      they can be cancelled
